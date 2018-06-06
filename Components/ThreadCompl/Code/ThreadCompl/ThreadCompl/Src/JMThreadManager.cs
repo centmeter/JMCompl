245661a0a9d2903e2e2db62c08806edbbd10605a
CM <centmeter@outlook.com>
2018-06-07 00:12:26 +0800

    1、更新 ThreadCompl 初步构建JM线程管理

---
+++ Components/ThreadCompl/Code/ThreadCompl/ThreadCompl/Src/JMThreadManager.cs
@@ -0,0 +1,184 @@
+﻿using System;
+
+namespace JM.ThreadCompl
+{
+    /// <summary>
+    /// 线程管理
+    /// </summary>
+    public class JMThreadManager
+    {
+        #region Singleton
+
+        private static JMThreadManager _instance;
+        private static object _locker = new object();
+        /// <summary>
+        /// 实例
+        /// </summary>
+        public static JMThreadManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_locker)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new JMThreadManager();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+        private JMThreadManager()
+        {
+
+        }
+
+        #endregion
+
+        #region Private Variable
+
+        /// <summary>
+        /// 初始化完成标识
+        /// </summary>
+        private bool _initDone = false;
+
+        /// <summary>
+        /// 线程池
+        /// </summary>
+        private JMThreadPool _threadPool;
+
+        #endregion
+        
+        #region Event
+
+        /// <summary>
+        /// 线程模块异常事件 arg:异常描述
+        /// </summary>
+        public event Action<string> OnExceptionEvent;
+
+        /// <summary>
+        /// 线程模块初始化成功事件
+        /// </summary>
+        public event Action OnInitSuccessEvent;
+
+        #endregion
+
+        #region Public Func
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public void Initialize(int maxCount = 25)
+        {
+            if (!_initDone)
+            {
+                _threadPool = new JMThreadPool(maxCount);
+                _initDone = true;
+                if (OnInitSuccessEvent != null)
+                {
+                    OnInitSuccessEvent.Invoke();
+                }
+            }
+            else
+            {
+                if (OnExceptionEvent != null)
+                {
+                    string arg = "## JM Error ## cls:JMThreadManager func:Initialize info:Has initialized";
+                    OnExceptionEvent.Invoke(arg);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 运行线程
+        /// </summary>
+        /// <param name="threadAct">线程方法</param>
+        /// <param name="onThreadCompletedCallback">线程完成回调</param>
+        /// <returns>线程唯一标识Id</returns>
+        public string RunThread(Action threadAct, Action onThreadCompletedCallback = null)
+        {
+            string id = string.Empty;
+            if (_initDone)
+            {
+                id = _threadPool.CreateThread(threadAct, onThreadCompletedCallback, (error) =>
+                {
+                    if (OnExceptionEvent != null)
+                    {
+                        OnExceptionEvent.Invoke(error);
+                    }
+                });
+            }
+            return id;
+        }
+
+        /// <summary>
+        /// 获取线程状态
+        /// </summary>
+        public ThreadStatus GetThreadStatus(string threadId)
+        {
+            ThreadStatus status = ThreadStatus.DoneOrNotExists;
+            if (_initDone)
+            {
+                status = _threadPool.GetThreadStatus(threadId);
+            }
+            return status;
+        }
+
+        /// <summary>
+        /// 强制结束线程
+        /// </summary>
+        public void AbortThread(string threadId)
+        {
+            if(_initDone)
+            {
+                _threadPool.DestroyThread(threadId, (error) =>
+                 {
+                     if(OnExceptionEvent!=null)
+                     {
+                         OnExceptionEvent.Invoke(error);
+                     }
+                 });
+            }
+        }
+
+        /// <summary>
+        /// 注销
+        /// </summary>
+        public void DoRelease()
+        {
+            if(_initDone)
+            {
+                OnExceptionEvent = null;
+                OnInitSuccessEvent = null;
+                _threadPool.Destroy();
+            }
+        }
+        #endregion
+    }
+
+    #region Enum
+
+    /// <summary>
+    /// 线程状态
+    /// </summary>
+    public enum ThreadStatus
+    {
+        /// <summary>
+        /// 等待
+        /// </summary>
+        Wait,
+        /// <summary>
+        /// 运行
+        /// </summary>
+        Run,
+        /// <summary>
+        /// 结束或者不存在
+        /// </summary>
+        DoneOrNotExists
+
+    }
+    #endregion
+}
---
+++ Components/ThreadCompl/Code/ThreadCompl/ThreadCompl/Src/JMThreadPool.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace JM.ThreadCompl
+{
+    internal class JMThreadPool
+    {
+        #region Private Variable
+
+        /// <summary>
+        /// 最大运行线程数
+        /// </summary>
+        private int _maxCount;
+
+        /// <summary>
+        /// 运行线程列表
+        /// </summary>
+        private List<Thread> _runList = new List<Thread>();
+
+        /// <summary>
+        /// 等待线程列表
+        /// </summary>
+        private List<Thread> _waitList = new List<Thread>();
+
+        /// <summary>
+        /// 运行线程列表锁
+        /// </summary>
+        private object _runListLocker = new object();
+
+        /// <summary>
+        /// 等待线程列表锁
+        /// </summary>
+        private object _waitListLocker = new object();
+
+        #endregion
+
+        #region Event
+
+        #endregion
+
+        #region Internal Func
+
+        internal JMThreadPool()
+        {
+            _maxCount = 25;
+        }
+
+        internal JMThreadPool(int maxCount)
+        {
+            _maxCount = maxCount;
+        }
+
+
+        /// <summary>
+        /// 创建线程
+        /// </summary>
+        internal string CreateThread(Action threadAct, Action onThreadCompletedCallback = null, Action<string> onThreadExceptionCallback = null)
+        {
+            string threadId = string.Format("JMThread_{0}", Guid.NewGuid());
+            Thread thread = new Thread(new ThreadStart(() =>
+            {
+                try
+                {
+                    if (threadAct != null)
+                    {
+                        threadAct.Invoke();
+                    }
+                    if (onThreadCompletedCallback != null)
+                    {
+                        onThreadCompletedCallback.Invoke();
+                    }
+                    OnRunThreadCompleted();
+                }
+                catch (Exception e)
+                {
+                    if (onThreadExceptionCallback != null)
+                    {
+                        string arg = string.Format("## JM Error ## cls:JMThreadPool func:CreateThread info:{0}", e.ToString());
+                        onThreadExceptionCallback.Invoke(arg);
+                    }
+                }
+            }));
+            thread.IsBackground = true;
+            thread.Name = threadId;
+
+            if (_runList.Count < _maxCount)
+            {
+                lock (_runListLocker)
+                {
+                    _runList.Add(thread);
+                }
+                thread.Start();
+            }
+            else
+            {
+                lock (_waitListLocker)
+                {
+                    _waitList.Add(thread);
+                }
+            }
+            return threadId;
+
+        }
+
+        /// <summary>
+        /// 获取线程状态
+        /// </summary>
+        internal ThreadStatus GetThreadStatus(string threadId)
+        {
+            ThreadStatus status = ThreadStatus.DoneOrNotExists;
+
+            Thread thread = GetRunThreadById(threadId);
+            if (thread != null)
+            {
+                status = ThreadStatus.Run;
+            }
+            else
+            {
+                thread = GetWaitThreadById(threadId);
+                if (thread != null)
+                {
+                    status = ThreadStatus.Wait;
+                }
+            }
+            return status;
+
+        }
+
+        /// <summary>
+        /// 销毁线程
+        /// </summary>
+        internal void DestroyThread(string threadId, Action<string> onDestroyThreadFailCallback = null)
+        {
+            Thread thread = GetRunThreadById(threadId);
+            if (thread != null)
+            {
+                lock (_runListLocker)
+                {
+                    _runList.Remove(thread);
+                }
+                thread.Abort();
+                thread = null;
+                RunWaitThread();
+            }
+            else
+            {
+                thread = GetWaitThreadById(threadId);
+                if (thread != null)
+                {
+                    lock (_waitListLocker)
+                    {
+                        _waitList.Remove(thread);
+                        thread.Abort();
+                        thread = null;
+                    }
+                }
+                else
+                {
+                    if (onDestroyThreadFailCallback != null)
+                    {
+                        string arg = string.Format("## JM Error ## cls:JMThreadPool func:DestroyThread info:Thread not exists");
+                        onDestroyThreadFailCallback.Invoke(arg);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 销毁
+        /// </summary>
+        internal void Destroy()
+        {
+            for (int i = 0; i < _runList.Count; i++)
+            {
+                Thread runThread = _runList[i];
+                if (runThread != null)
+                {
+                    runThread.Abort();
+                    runThread = null;
+                }
+            }
+            for (int i = 0; i < _waitList.Count; i++)
+            {
+                Thread waitThread = _waitList[i];
+                if (waitThread != null)
+                {
+                    waitThread.Abort();
+                    waitThread = null;
+                }
+            }
+            _runList.Clear();
+            _waitList.Clear();
+            _runList = null;
+            _waitList = null;
+
+        }
+
+        #endregion
+
+        #region Private Func
+
+        /// <summary>
+        /// 线程运行结束处理
+        /// </summary>
+        private void OnRunThreadCompleted()
+        {
+            Thread thread = Thread.CurrentThread;
+            if (_runList.Contains(thread))
+            {
+                lock (_runListLocker)
+                {
+                    _runList.Remove(thread);
+                }
+            }
+            RunWaitThread();
+        }
+
+        /// <summary>
+        /// 运行等待线程
+        /// </summary>
+        private void RunWaitThread()
+        {
+            if (_waitList.Count > 0)
+            {
+                _waitList[0].Start();
+                lock (_runListLocker)
+                {
+                    _runList.Add(_waitList[0]);
+                }
+                lock (_waitListLocker)
+                {
+                    _waitList.RemoveAt(0);
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// 通过线程唯一标识Id获取运行线程实例
+        /// </summary>
+        /// <param name="threadId"></param>
+        /// <returns></returns>
+        private Thread GetRunThreadById(string threadId)
+        {
+            Thread thread = null;
+            for (int i = 0; i < _runList.Count; i++)
+            {
+                if (_runList[i].Name.Equals(threadId))
+                {
+                    thread = _runList[i];
+                    break;
+                }
+            }
+            return thread;
+        }
+
+        /// <summary>
+        /// 通过线程唯一标识Id获取等待线程实例
+        /// </summary>
+        private Thread GetWaitThreadById(string threadId)
+        {
+            Thread thread = null;
+            for (int i = 0; i < _waitList.Count; i++)
+            {
+                if (_waitList[i].Name.Equals(threadId))
+                {
+                    thread = _waitList[i];
+                    break;
+                }
+            }
+            return thread;
+        }
+
+        /// <summary>
+        /// 通过线程唯一标识Id获取线程实例
+        /// </summary>
+        private Thread GetThreadById(string threadId)
+        {
+            Thread thread = GetRunThreadById(threadId);
+            if (thread == null)
+            {
+                thread = GetWaitThreadById(threadId);
+            }
+            return thread;
+        }
+        #endregion
+    }
+}
