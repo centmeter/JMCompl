dd1fa527bb7c920e49a700e5baac83b91ce76afb
CM <centmeter@outlook.com>
2018-08-15 20:31:12 +0800

    1、JMUISearcher使用Unity2017.4.5f1 做Demo 2、JMUISearcher完成=UIRoot和Canvas为根节点的两个路径搜索功能

--- Components/UISearcher/Code/JMUISearcher/JMUISearcher/Src/JMUISeacher.cs
+++ Components/UISearcher/Code/JMUISearcher/JMUISearcher/Src/JMUISeacher.cs
@@ -8,22 +8,56 @@ namespace JMEditor.UISearcher
         #region Public Func
 
         /// <summary>
-        /// 查询UI节点路径(复制到剪切板)
+        /// 查询UI节点路径(复制到剪切板) 相对UI根节点(不包括根节点)
         /// </summary>
-        [MenuItem("GameObject/JMEditor/UISearcher", false, 11)]
-        public static void Search()
+        [MenuItem("GameObject/JMEditor/UISearcher(UIRoot)", false, 11)]
+        public static void SearchByRoot()
         {
-            Transform selectedTrans = Selection.activeTransform;
-            string path = string.Empty;
-            GetNodePath(selectedTrans, ref path);
-            Debug.LogFormat("## Uni Log ## Author:<Ming> Cls:JMUISeacher Func:Search Node:{0} Path:{1}", selectedTrans.name, path);
+            string path = Search();
+            CutString("/", 2, ref path);
+            if (!string.IsNullOrEmpty(path))
+            {
+                Debug.LogFormat("## Uni Log <Ming> ## Copy Done: {0}", path);
+                CopyContent(path);
+            }
+            else
+            {
+                Debug.Log("<color=yellow>## Uni Warning <Ming>## Path Empty</color>");
+            }
+        }
 
-
+        /// <summary>
+        /// 查询UI节点路径(复制到剪切板) 相对Canvas节点(包括Canvas节点)
+        /// </summary>
+        [MenuItem("GameObject/JMEditor/UISearcher(Canvas)", false, 11)]
+        public static void SearchByCanvas()
+        {
+            string path = Search();
+            if (!string.IsNullOrEmpty(path))
+            {
+                Debug.LogFormat("## Uni Log <Ming> ## Copy Done: {0}", path);
+                CopyContent(path);
+            }
+            else
+            {
+                Debug.Log("<color=yellow>## Uni Warning <Ming>## Path Empty</color>");
+            }
         }
 
         #endregion
 
         #region Private Func
+
+        /// <summary>
+        /// 搜索节点返回路径
+        /// </summary>
+        private static string Search()
+        {
+            Transform selectedTrans = Selection.activeTransform;
+            string path = string.Empty;
+            GetNodePath(selectedTrans, ref path);
+            return path;
+        }
 
         /// <summary>
         /// 获取节点路径
@@ -33,8 +67,7 @@ namespace JMEditor.UISearcher
             if (nodeTrans != null)
             {
                 string curNodeName = nodeTrans.name;
-                path = string.Format("{0}/{1}", curNodeName, path);
-
+                path = string.Format("{0}{1}", curNodeName, string.IsNullOrEmpty(path) ? string.Empty : string.Format("/{0}", path));
                 Transform parentNode = nodeTrans.parent;
                 if (parentNode != null)
                 {
@@ -46,7 +79,7 @@ namespace JMEditor.UISearcher
         /// <summary>
         /// 复制字符串进剪切板
         /// </summary>
-        private void CopyContent(string content)
+        private static void CopyContent(string content)
         {
             TextEditor textEditor = new TextEditor();
             textEditor.text = content;
@@ -54,7 +87,27 @@ namespace JMEditor.UISearcher
             textEditor.Copy();
         }
 
+        /// <summary>
+        /// 根据标识截断字符串
+        /// </summary>
+        private static void CutString(string cutFlag, int flagCount, ref string content)
+        {
+            if (flagCount > 0)
+            {
+                int flagIndex = content.IndexOf(cutFlag);
 
+                if (flagIndex >= 0)
+                {
+                    content = content.Substring(flagIndex + 1);
+                    CutString(cutFlag, --flagCount, ref content);
+                }
+                else
+                {
+                    content = string.Empty;
+                }
+
+            }
+        }
         #endregion
 
     }
